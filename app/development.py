6c07132cbfb13ade78e09e0ba65f174091b84684
SlZeroth <qkrwncks593@gmail.com>
2020-06-22 14:05:30 +0900

    first commit

---
+++ app/development.py
@@ -0,0 +1,16 @@
+from .settings import *
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'test_db',
+        'USER': 'root',
+        'PASSWORD': '593Doq66!!',
+        'PORT': '3306',
+        'HOST': '34.64.188.81',
+        'CONN_MAX_AGE': 500,
+        'OPTIONS': {
+            'charset': 'utf8mb4'
+        }
+    }
+}
